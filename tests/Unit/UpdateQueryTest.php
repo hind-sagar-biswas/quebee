24b8baf6951fdf8fb3d8c6189ad19d2b396dc966
Hind Sagar Biswas <hindsbhk@gmail.com>
2023-09-21 15:55:18 +0600

    added exceptions test

--- tests/Unit/UpdateQueryTest.php
+++ tests/Unit/UpdateQueryTest.php
@@ -9,9 +9,28 @@ final class UpdateQueryTest extends TestCase
 {
     public function test_update_query_build()
     {
-        $expected = "UPDATE users SET name = 'Jane', age = '20' WHERE id = '53';";
-        $data = [ 'name' => 'Jane', 'age' => 20, ];
+        $expected = "UPDATE users SET name = 'Jane', age = '20', affiliation = NULL WHERE id = '53';";
+        $data = [ 'name' => 'Jane', 'age' => 20, 'affiliation' => null ];
         $query = Query::update('users')->set($data)->where('id', '=', '53')->build();
         $this->assertSame($expected, $query);
+    }
+
+    public function test_empty_data_exception()
+    {
+        $this->expectException(InvalidArgumentException::class);
+        Query::update('users')->set([])->build();
+    }
+
+    public function test_non_assoc_data_exception()
+    {
+        $this->expectException(InvalidArgumentException::class);
+        Query::update('users')->set(['Jane', 0])->build();
+    }
+
+    public function test_no_where_clause_exception()
+    {
+        $this->expectException(BadMethodCallException::class);
+        $data = ['name' => 'Jane', 'age' => 20, 'affiliation' => null];
+        Query::update('users')->set($data)->build();
     }
 }
