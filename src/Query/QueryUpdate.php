db313929216d12fe7489709bcb5353ff3849873f
Hind Sagar Biswas <hindsbhk@gmail.com>
2023-09-21 15:49:14 +0600

    Completed class for update query

--- src/Query/QueryUpdate.php
+++ src/Query/QueryUpdate.php
@@ -5,14 +5,29 @@ declare(strict_types=1);
 namespace Hindbiswas\QueBee\Query;
 
 use Hindbiswas\QueBee\Clause\WhereClause;
+use Hindbiswas\QueBee\Query;
 use Hindbiswas\QueBee\Query\QueryStruct;
 
 class QueryUpdate implements QueryStruct
 {
     use WhereClause;
 
+    protected array $data;
+
+    public function __construct(protected readonly string $table) {}
+
+    public function set(array $data): self
+    {
+        if (empty($data)) throw new \InvalidArgumentException('No data provided. Data array cannot be empty.');
+        if (array_is_list($data)) throw new \InvalidArgumentException('Data must be an associative array.');
+        $this->data = $data;
+        return $this;
+    }
+
     public function build(): string
     {
-        return '';
+        if (!$this->where) throw new \BadMethodCallException('No Where clause added. Updates must have conditions.');
+
+        return 'UPDATE '. $this->table . ' SET ' . Query::flattenByEqual($this->data) . ' WHERE '. $this->where . ';';
     }
 }
