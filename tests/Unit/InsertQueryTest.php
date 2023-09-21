c3814ef207f499a36267120c5b81d2c08cd7b36d
Hind Sagar Biswas <hindsbhk@gmail.com>
2023-09-21 15:11:36 +0600

    added insert tests

--- tests/Unit/InsertQueryTest.php
+++ tests/Unit/InsertQueryTest.php
@@ -39,9 +39,37 @@ final class InsertQueryTest extends TestCase
         $this->assertSame($expected, $query);
 
         // For single array in multiple data
-        $data = [ $this->data[0] ];
+        $data = [$this->data[0]];
         $expected = "INSERT INTO users(id, name, age) VALUES ('1', 'John', '30');";
         $query = Query::insertMultiple($data)->into('users')->build();
         $this->assertSame($expected, $query);
+    }
+
+
+    public function test_empty_data_insert_exception()
+    {
+        $this->expectException(InvalidArgumentException::class);
+        Query::insert([])->into('users')->build();
+    }
+
+
+    public function test_data_non_assoc_insert_exception()
+    {
+        $this->expectException(InvalidArgumentException::class);
+        Query::insert([1, 2, 3])->into('users')->build();
+    }
+
+
+    public function test_empty_data_multiple_insert_single_exception()
+    {
+        $this->expectException(InvalidArgumentException::class);
+        Query::insert([['name' => 'John'], [], ['name' => 'Jane']])->into('users')->build();
+    }
+
+
+    public function test_multiple_data_non_assoc_insert_single_exception()
+    {
+        $this->expectException(InvalidArgumentException::class);
+        Query::insert([['John'], ['Jane']])->into('users')->build();
     }
 }
