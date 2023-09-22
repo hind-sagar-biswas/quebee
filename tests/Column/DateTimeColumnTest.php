8751df5799c9efaaa6885143bffa8d9a3d5bb050
Hind Sagar Biswas <hindsbhk@gmail.com>
2023-09-22 14:24:33 +0600

    added date time tests

---
+++ tests/Column/DateTimeColumnTest.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+use Hindbiswas\QueBee\Col;
+use Hindbiswas\QueBee\Table\Values\DefaultVal;
+use PHPUnit\Framework\TestCase;
+
+final class DateTimeColumnTest extends TestCase
+{
+    public function test_date_column_build()
+    {
+        // Basic DATE column
+        $expected = "`column` DATE NOT NULL";
+        $query = Col::date()->build('column');
+        $this->assertSame($expected, $query);
+       
+        // Nullable DATE column
+        $expected = "`column` DATE NULL";
+        $query = Col::date()->nullable()->build('column');
+        $this->assertSame($expected, $query);
+    }
+   
+    public function test_datetime_column_build()
+    {
+        // Basic DATETIME column
+        $expected = "`column` DATETIME NOT NULL";
+        $query = Col::dateTime()->build('column');
+        $this->assertSame($expected, $query);
+
+        // onupdate DATETIME column
+        $expected = "`column` DATETIME on update CURRENT_TIMESTAMP NOT NULL";
+        $query = Col::dateTime()->setOnUpdate()->build('column');
+        $this->assertSame($expected, $query);
+
+        // DATETIME column with default CURRENT_TIME
+        $expected = "`column` DATETIME NOT NULL DEFAULT CURRENT_TIME";
+        $query = Col::dateTime()->default(DefaultVal::CURRENT_TIME)->build('column');
+        $this->assertSame($expected, $query);
+
+        // Nullable DATETIME column with default NULL
+        $expected = "`column` DATETIME NULL DEFAULT NULL";
+        $query = Col::dateTime()->default(DefaultVal::NULL)->build('column');
+        $this->assertSame($expected, $query);
+    }
+}
