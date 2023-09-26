b64c73fba9ab9a83b953c98e5e42f37ba0d81973
Hind Sagar Biswas <hindsbhk@gmail.com>
2023-09-26 14:26:26 +0600

    completed basic create table | except foreign

--- src/Table/CreateTable.php
+++ src/Table/CreateTable.php
@@ -4,18 +4,56 @@ declare(strict_types=1);
 
 namespace Hindbiswas\QueBee\Table;
 
-class CreateTable {
-    protected array $columns = [];
+use Hindbiswas\QueBee\Table\Column\ColumnInterface;
+
+class CreateTable
+{
+    protected array $columnList;
 
     public function __construct(public readonly string $name)
-    {}
+    {
+    }
 
-    public function columns(array $columns): self {
-        $this->columns = $columns;
+    public function columns(array $columns): self
+    {
+        if (array_is_list($columns)) throw new \InvalidArgumentException("Columns must be associative array");
+        $this->columnList = $columns;
         return $this;
     }
 
-    public function foreigns(array $constrains): self {
+    public function foreigns(array $constrains): self
+    {
         return $this;
+    }
+
+    public function build(): string
+    {
+        $columns = [];
+        $constrains = [
+            'UNIQUE' => [],
+            'PRIMARY KEY' => [],
+        ];
+        foreach ($this->columnList as $columnName => $columnObject) {
+            if ($columnObject instanceof ColumnInterface) {
+                if ($columnObject->getConstraints()) $constrains[$columnObject->getConstraints()][] = $columnName;
+                $columns[] = $columnObject->build($columnName);
+            }
+        }
+
+        if (!empty($constrains['PRIMARY KEY'])) {
+            $keyName = $this->name . "_PK";
+            $constraintStr = "CONSTRAINT $keyName PRIMARY KEY (" . implode(', ', $constrains['PRIMARY KEY']) . ")";
+            $columns[] = $constraintStr;
+        }
+
+        foreach ($constrains['UNIQUE'] as $columnName) {
+            $keyName = $columnName . "_UC";
+            $constraintStr = "CONSTRAINT $keyName UNIQUE (`" . $columnName . "`)";
+            $columns[] = $constraintStr;
+        }
+
+        $sql = "CREATE TABLE IF NOT EXISTS $this->name (" . implode(', ', $columns) . ") ENGINE = InnoDB;";
+
+        return $sql;
     }
 }
