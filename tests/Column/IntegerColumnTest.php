da05d29309203385947a9e5caeb80136b1d22ca9
Hind Sagar Biswas <hindsbhk@gmail.com>
2023-11-01 02:18:25 +0600

    added corresponding tests

--- tests/Column/IntegerColumnTest.php
+++ tests/Column/IntegerColumnTest.php
@@ -17,7 +17,7 @@ final class IntegerColumnTest extends TestCase
 
         /// length specified zerofill INT column
         $expected = "`column` INT(11) ZEROFILL NOT NULL";
-        $query = Col::integer(11)->build('column');
+        $query = Col::integer(11)->zerofill()->build('column');
         $this->assertSame($expected, $query);
 
         /// Nullable INT column with default NULL
@@ -31,8 +31,11 @@ final class IntegerColumnTest extends TestCase
         $this->assertSame($expected, $query);
 
         /// Unsigned zerofill INT column
-        $expected = "`column` INT UNSIGNED NOT NULL";
-        $query = Col::integer()->unsigned(true)->build('column');
+        $expected = "`column` INT(11) UNSIGNED ZEROFILL NOT NULL";
+        $query = Col::integer(11)->unsigned()->zerofill()->build('column');
+        $this->assertSame($expected, $query);
+        $expected = "`column` INT(11) ZEROFILL UNSIGNED NOT NULL";
+        $query = Col::integer(11)->zerofill()->unsigned()->build('column');
         $this->assertSame($expected, $query);
     }
 
@@ -40,27 +43,30 @@ final class IntegerColumnTest extends TestCase
     {
         /// Basic BIGINT column
         $expected = "`column` BIGINT NOT NULL";
-        $query = Col::bigInt()->build('column');
+        $query = Col::integer()->build('column');
         $this->assertSame($expected, $query);
 
-        /// length specified BIGINT column
-        $expected = "`column` BIGINT(16) NOT NULL";
-        $query = Col::bigInt(16)->build('column');
+        /// length specified zerofill BIGINT column
+        $expected = "`column` BIGINT(20) ZEROFILL NOT NULL";
+        $query = Col::integer(20)->zerofill()->build('column');
         $this->assertSame($expected, $query);
 
         /// Nullable BIGINT column with default NULL
         $expected = "`column` BIGINT NULL DEFAULT NULL";
-        $query = Col::bigInt()->nullable()->default(DefaultVal::NULL)->build('column');
+        $query = Col::integer()->nullable()->default(DefaultVal::NULL)->build('column');
         $this->assertSame($expected, $query);
 
         /// Unsigned BIGINT column
         $expected = "`column` BIGINT UNSIGNED NOT NULL";
-        $query = Col::bigInt()->unsigned()->build('column');
+        $query = Col::integer()->unsigned()->build('column');
         $this->assertSame($expected, $query);
 
         /// Unsigned zerofill BIGINT column
-        $expected = "`column` BIGINT UNSIGNED ZEROFILL NOT NULL";
-        $query = Col::bigInt()->unsigned(true)->build('column');
+        $expected = "`column` BIGINT(20) UNSIGNED ZEROFILL NOT NULL";
+        $query = Col::integer(20)->unsigned()->zerofill()->build('column');
+        $this->assertSame($expected, $query);
+        $expected = "`column` BIGINT(20) ZEROFILL UNSIGNED NOT NULL";
+        $query = Col::integer(20)->zerofill()->unsigned()->build('column');
         $this->assertSame($expected, $query);
     }
 
@@ -68,27 +74,30 @@ final class IntegerColumnTest extends TestCase
     {
         /// Basic MEDIUMINT column
         $expected = "`column` MEDIUMINT NOT NULL";
-        $query = Col::mediumInt()->build('column');
+        $query = Col::integer()->build('column');
         $this->assertSame($expected, $query);
 
-        /// length specified MEDIUMINT column
-        $expected = "`column` MEDIUMINT(16) NOT NULL";
-        $query = Col::mediumInt(16)->build('column');
+        /// length specified zerofill MEDIUMINT column
+        $expected = "`column` MEDIUMINT(15) ZEROFILL NOT NULL";
+        $query = Col::integer(15)->zerofill()->build('column');
         $this->assertSame($expected, $query);
 
         /// Nullable MEDIUMINT column with default NULL
         $expected = "`column` MEDIUMINT NULL DEFAULT NULL";
-        $query = Col::mediumInt()->nullable()->default(DefaultVal::NULL)->build('column');
+        $query = Col::integer()->nullable()->default(DefaultVal::NULL)->build('column');
         $this->assertSame($expected, $query);
 
         /// Unsigned MEDIUMINT column
         $expected = "`column` MEDIUMINT UNSIGNED NOT NULL";
-        $query = Col::mediumInt()->unsigned()->build('column');
+        $query = Col::integer()->unsigned()->build('column');
         $this->assertSame($expected, $query);
 
         /// Unsigned zerofill MEDIUMINT column
-        $expected = "`column` MEDIUMINT UNSIGNED ZEROFILL NOT NULL";
-        $query = Col::mediumInt()->unsigned(true)->build('column');
+        $expected = "`column` MEDIUMINT(15) UNSIGNED ZEROFILL NOT NULL";
+        $query = Col::integer(15)->unsigned()->zerofill()->build('column');
+        $this->assertSame($expected, $query);
+        $expected = "`column` MEDIUMINT(15) ZEROFILL UNSIGNED NOT NULL";
+        $query = Col::integer(15)->zerofill()->unsigned()->build('column');
         $this->assertSame($expected, $query);
     }
 
@@ -96,27 +105,30 @@ final class IntegerColumnTest extends TestCase
     {
         /// Basic SMALLINT column
         $expected = "`column` SMALLINT NOT NULL";
-        $query = Col::smallInt()->build('column');
+        $query = Col::integer()->build('column');
         $this->assertSame($expected, $query);
 
-        /// length specified SMALLINT column
-        $expected = "`column` SMALLINT(6) NOT NULL";
-        $query = Col::smallInt(6)->build('column');
+        /// length specified zerofill SMALLINT column
+        $expected = "`column` SMALLINT(6) ZEROFILL NOT NULL";
+        $query = Col::integer(6)->zerofill()->build('column');
         $this->assertSame($expected, $query);
 
         /// Nullable SMALLINT column with default NULL
         $expected = "`column` SMALLINT NULL DEFAULT NULL";
-        $query = Col::smallInt()->nullable()->default(DefaultVal::NULL)->build('column');
+        $query = Col::integer()->nullable()->default(DefaultVal::NULL)->build('column');
         $this->assertSame($expected, $query);
 
         /// Unsigned SMALLINT column
         $expected = "`column` SMALLINT UNSIGNED NOT NULL";
-        $query = Col::smallInt()->unsigned()->build('column');
+        $query = Col::integer()->unsigned()->build('column');
         $this->assertSame($expected, $query);
 
         /// Unsigned zerofill SMALLINT column
-        $expected = "`column` SMALLINT UNSIGNED ZEROFILL NOT NULL";
-        $query = Col::smallInt()->unsigned(true)->build('column');
+        $expected = "`column` SMALLINT(6) UNSIGNED ZEROFILL NOT NULL";
+        $query = Col::integer(6)->unsigned()->zerofill()->build('column');
+        $this->assertSame($expected, $query);
+        $expected = "`column` SMALLINT(6) ZEROFILL UNSIGNED NOT NULL";
+        $query = Col::integer(6)->zerofill()->unsigned()->build('column');
         $this->assertSame($expected, $query);
     }
 
@@ -124,27 +136,30 @@ final class IntegerColumnTest extends TestCase
     {
         /// Basic TINYINT column
         $expected = "`column` TINYINT NOT NULL";
-        $query = Col::tinyInt()->build('column');
+        $query = Col::integer()->build('column');
         $this->assertSame($expected, $query);
 
-        /// length specified TINYINT column
-        $expected = "`column` TINYINT(2) NOT NULL";
-        $query = Col::tinyInt(2)->build('column');
+        /// length specified zerofill TINYINT column
+        $expected = "`column` TINYINT(2) ZEROFILL NOT NULL";
+        $query = Col::integer(2)->zerofill()->build('column');
         $this->assertSame($expected, $query);
 
         /// Nullable TINYINT column with default NULL
         $expected = "`column` TINYINT NULL DEFAULT NULL";
-        $query = Col::tinyInt()->nullable()->default(DefaultVal::NULL)->build('column');
+        $query = Col::integer()->nullable()->default(DefaultVal::NULL)->build('column');
         $this->assertSame($expected, $query);
 
         /// Unsigned TINYINT column
         $expected = "`column` TINYINT UNSIGNED NOT NULL";
-        $query = Col::tinyInt()->unsigned()->build('column');
+        $query = Col::integer()->unsigned()->build('column');
         $this->assertSame($expected, $query);
 
         /// Unsigned zerofill TINYINT column
-        $expected = "`column` TINYINT UNSIGNED ZEROFILL NOT NULL";
-        $query = Col::tinyInt()->unsigned(true)->build('column');
+        $expected = "`column` TINYINT(2) UNSIGNED ZEROFILL NOT NULL";
+        $query = Col::integer(2)->unsigned()->zerofill()->build('column');
+        $this->assertSame($expected, $query);
+        $expected = "`column` TINYINT(2) ZEROFILL UNSIGNED NOT NULL";
+        $query = Col::integer(2)->zerofill()->unsigned()->build('column');
         $this->assertSame($expected, $query);
     }
 }
