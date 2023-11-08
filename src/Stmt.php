0fca29c14688511cf472f87e3f971fc7f773bcde
Hind Sagar Biswas <hindsbhk@gmail.com>
2023-11-09 01:44:12 +0600

    INC: included Cube stmt to main class

--- src/Stmt.php
+++ src/Stmt.php
@@ -6,6 +6,7 @@ namespace Hindbiswas\QueBee;
 
 use Hindbiswas\QueBee\Stmt\UnionStmt;
 use Hindbiswas\QueBee\Query\QuerySelect;
+use Hindbiswas\QueBee\Stmt\CubeStmt;
 
 class Stmt
 {
@@ -17,5 +18,10 @@ class Stmt
     public static function unionAll(QuerySelect ...$select_queries): UnionStmt
     {
         return new UnionStmt(true, ...$select_queries);
+    }
+    
+    public static function cube(array $columns): CubeStmt
+    {
+        return new CubeStmt($columns);
     }
 }
